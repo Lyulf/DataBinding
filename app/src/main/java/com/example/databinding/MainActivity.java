4e5bc5cd2c9dea90b07f78f7956f4c15acaeda65
Lyulf <patryk.chodorowski0@gmail.com>
2019-05-07 21:05:31 +0200

    Created example without databinding for demonstration

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -1,34 +1,67 @@
 package com.example.databinding;
 
-import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.databinding.databinding.ActivityMainBinding;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    TemperatureData temp;
+    TemperatureData temperature;
+    Button refreshButton;
+    TextView celsiusTextView;
+    TextView locationTextView;
+    EditText celsiusEditText;
+    EditText locationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        temp = new TemperatureData("Lodz", 10.0);
+        setContentView(R.layout.activity_main);
+        temperature = new TemperatureData("Lodz", 10.0);
         if(savedInstanceState != null) {
             String location = savedInstanceState.getString("location");
             Double celsius = savedInstanceState.getDouble("celsius");
-            temp.setLocation(location);
-            temp.setCelsius(celsius);
+            temperature.setLocation(location);
+            temperature.setCelsius(celsius);
         }
-        binding.setTemp(temp);
+
+        refreshButton = findViewById(R.id.Refresh_Button);
+        celsiusTextView = findViewById(R.id.Celsius_TextView);
+        locationTextView = findViewById(R.id.Location_TextView);
+        celsiusEditText = findViewById(R.id.Celsius_EditText);
+        locationEditText = findViewById(R.id.Location_EditText);
+
+        updateValues();
+
+        refreshButton.setOnClickListener(
+            (View view) -> {
+                double celsius;
+                try {
+                    celsius = Double.parseDouble(celsiusEditText.getText().toString());
+                } catch(NumberFormatException e) {
+                    celsius = 0.0;
+                }
+                temperature.setCelsius(celsius);
+                temperature.setLocation(locationEditText.getText().toString());
+                updateValues();
+            }
+        );
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         if(savedInstanceState != null) {
-            savedInstanceState.putString("location", temp.getLocation());
-            savedInstanceState.putDouble("celsius", temp.getCelsius());
+            savedInstanceState.putString("location", temperature.getLocation());
+            savedInstanceState.putDouble("celsius", temperature.getCelsius());
         }
         super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void updateValues() {
+        celsiusTextView.setText(getString(R.string.temperature) + ": " + temperature.getCelsius() + "°C");
+        locationTextView.setText(getString(R.string.location) + ": " + temperature.getLocation());
     }
 }
--- app/src/main/java/com/example/databinding/TemperatureData.java
+++ app/src/main/java/com/example/databinding/TemperatureData.java
@@ -1,44 +1,27 @@
 package com.example.databinding;
 
-import android.databinding.BaseObservable;
-import android.databinding.Bindable;
-
-public class TemperatureData extends BaseObservable{
+public class TemperatureData {
+    private double celsius;
     private String location;
-    private Double celsius;
 
-    TemperatureData(String location, Double celsius) {
+    TemperatureData(String location, double celsius) {
         this.location = location;
         this.celsius = celsius;
     }
 
-    @Bindable
-    public Double getCelsius() {
+    double getCelsius() {
         return celsius;
     }
 
-    @Bindable
-    public String getLocation() {
+    void setCelsius(double celsius) {
+        this.celsius = celsius;
+    }
+
+    String getLocation() {
         return location;
     }
 
-    public void setCelsius(Double celsius) {
-        this.celsius = celsius;
-        notifyPropertyChanged(BR.celsius);
-    }
-
-    public void setLocation(String location) {
+    void setLocation(String location) {
         this.location = location;
-        notifyPropertyChanged(BR.location);
-    }
-
-    public void incrementCelsius() {
-        celsius++;
-        notifyPropertyChanged(BR.celsius);
-    }
-
-    public void decrementCelsius() {
-        celsius--;
-        notifyPropertyChanged(BR.celsius);
     }
 }
